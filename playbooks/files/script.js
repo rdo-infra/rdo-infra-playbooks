da681d8d977046ec65333ffee86577bfc4a0683d
Javier Pena <jpena@redhat.com>
2018-10-02 15:49:04 +0200

    RDO Infra inventory in Ansible
    This commit proposes a method to encode certain inventory data for
    each RDO Infra server as part of the Ansible inventory:
    
    - A host_vars/<hostname>/inventory.yml file, with a set of predefined
      tags.
    - A playbook that will gather facter information from the servers,
      aggregate it with the predefined tags, and generate a simple HTML
      report with the inventory.
    
    There is included documentation, so it can be used to add new servers
    in the future, as well as a simple CSS file to show the generated tables.
    
    TG-1786
    
    Change-Id: I54fd9fdebd3b680afc5a895437f7acec424ed2e8

---
+++ playbooks/files/script.js
@@ -0,0 +1,10 @@
+(function($){
+$(document).ready(function(){
+
+    $(".sidebar-left li a").click(function(e){
+        e.preventDefault(); //To prevent the default anchor tag behaviour
+        var url = this.href;
+        $(".main").load(url);
+    }); 
+});
+})(jQuery);
